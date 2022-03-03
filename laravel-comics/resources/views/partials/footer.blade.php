e3fbc70430000433168d6d4d738518582453b2cf
perezalexreal <perezalexreal@gmail.com>
2022-03-03 17:39:39 +0100

    Header e Footer finiti , contenuto main da inserire ( I fumetti )

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,1 +1,54 @@
-<h1>Footer</h1>
+<div class="footer">
+    <div class="main-footer">
+        <div class="footer-nav">
+            <h1>DC Comics</h1>
+            <p>Characters</p>
+            <p>Movies</p>
+            <p>Tv</p>
+            <p>Games</p>
+            <p>Videos</p>
+            <p>News</p>
+        </div>
+        <div class="footer-nav">
+            <h1>dc</h1>
+            <p>Terms Of User</p>
+            <p>Privacy Policy (New) </p>
+            <p>Ad Choices</p>
+            <p>Avertising</p>
+            <p>Jobs</p>
+            <p>Subscriptions</p>
+            <p>Talent Worshops</p>
+            <p>CPSC Certificates</p>
+            <p>Ratings</p>
+            <p>Shop Help</p>
+            <p>Contact Us</p>
+        </div>
+        <div class="footer-nav">
+            <h1>Sites</h1>
+            <p>DC</p>
+            <p>MAD Magazine</p>
+            <p>DC Kids</p>
+            <p>DC Universe</p>
+            <p>DC Power Visa</p>
+        </div>
+        <div class="footer-nav">
+            <h1>shop</h1>
+            <p>Shop DC</p>
+            <p>Shop DC Collectibles</p>
+        </div>
+     
+    </div>
+       <img class="logo-footer" src="{{ asset('/img/dc-logo-bg.png') }}" alt="">
+</div>
+
+<div class="footer-bottom">
+    <a href="#"><button class="button-sign-up">SIGN-UP-NOW</button></a> 
+      <div class="container-logos">
+          <h1>FOLLOW US</h1>
+          <img src="{{ asset('/img/footer-facebook.png') }}" alt="">
+          <img src="{{ asset('/img/footer-periscope.png') }}" alt="">
+          <img src="{{ asset('/img/footer-pinterest.png') }}" alt="">
+          <img src="{{ asset('/img/footer-twitter.png') }}" alt="">
+          <img src="{{ asset('/img/footer-youtube.png') }}" alt="">
+      </div>
+  </div>
--- laravel-comics/resources/views/partials/header.blade.php
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -1,1 +1,18 @@
-<h1>Header</h1>
+<header class="header">
+    <img class="logo-image" src="{{ asset('img/dc-logo.png') }}" alt="">
+    <div>
+        <ul>
+            <li> Characters</li>
+            <li> Comics</li>
+            <li> Movies</li>
+            <li> Tv</li>
+            <li> Games</li>
+            <li> Collectibles</li>
+            <li> Videos</li>
+            <li> Fans</li>
+            <li> News</li>
+            <li> Shop</li>
+        </ul>
+
+    </div>
+</header>
